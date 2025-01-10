5b496c1eda8e6eeba79a1c1436b0a0a2eb54b686
sebsteege <73400151+sebsteege@users.noreply.github.com>
2025-01-10 15:02:01 -0500

    Update ping_sweep_hosts.py

--- ping_sweep_hosts.py
+++ ping_sweep_hosts.py
@@ -2,30 +2,15 @@ import datetime
 import ipaddress
 from ping3 import ping
 
-print('Enter the subnet range you would like to ping:')
-host = input()
+print('Enter the hosts you would like to ping:')
+host_file = input()
 print('Enter the name of the output file:')
 output_file = input()
 
 def ping_sweep(subnet):
     network = ipaddress.ip_network(subnet, strict=False)
 
-    with open(host)
-
-    with open(output_file, 'a') as f:
-        for ip in network.hosts():
-            ip_str = str(ip)
-            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            message = ""
-            try:
-                response_time = ping(ip_str, timeout=1)
-                if response_time is not None:
-                    message = f"{timestamp} - {ip_str} is reachable"
-                else:
-                    message = f"{timestamp} - {ip_str} is unreachable"
-            except Exception:
-                message = f"{timestamp} - Error pinging {ip_str}"
-            print(message)
-            f.write(message + '\n')
-
-ping_sweep(subnet)
+    with open(host_file) as file:
+         for line in file:
+             for value in line.split():
+                 print(value)
