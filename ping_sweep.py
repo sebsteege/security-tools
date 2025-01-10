8e157bc97a162d55f38c45e80244ea7edf822a74
sebsteege <seb.steege@gmail.com>
2025-01-09 19:36:41 -0500

    Added input functions

---
+++ ping_sweep.py
@@ -0,0 +1,29 @@
+import datetime
+import ipaddress
+from ping3 import ping
+
+print('Enter the subnet range you would like to ping:')
+subnet = input()
+print('Enter the name of the output file:')
+output_file = input()
+
+def ping_sweep(subnet):
+    network = ipaddress.ip_network(subnet, strict=False)
+
+    with open(output_file, 'a') as f:
+        for ip in network.hosts():
+            ip_str = str(ip)
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            message = ""
+            try:
+                response_time = ping(ip_str, timeout=1)
+                if response_time is not None:
+                    message = f"{timestamp} - {ip_str} is reachable"
+                else:
+                    message = f"{timestamp} - {ip_str} is unreachable"
+            except Exception:
+                message = f"{timestamp} - Error pinging {ip_str}"
+            print(message)
+            f.write(message + '\n')
+
+ping_sweep(subnet)
