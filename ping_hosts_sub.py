fcb549e0ff9b219aaf700df53f97c01cad975801
sebsteege <seb.steege@gmail.com>
2025-01-20 10:30:46 -0500

    added ping_hosts_sub which utilizes subprocess. added functions to differentiate between platform os

---
+++ ping_hosts_sub.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+import platform
+import ipaddress
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+"""
+This script is used to ping hosts from a Windows system.
+Enter the full path to your hosts file if it exists in
+another directory.
+"""
+
+print()
+print('Enter the text file of hosts you would like to ping:')
+host_file = input()
+print()
+print('Enter the name of the output file:')
+output_file = input()
+print()
+
+def ping_host_windows(host):
+
+    try:
+        result = subprocess.run(['ping', '-c', '4', '-n', host], stdout=subprocess.DEVNULL, encoding='utf-8')
+        if "Destination host unreachable" in result.decode:
+            return f"{host} unreachable\n"
+        if "Request timed out" in result.decode:
+            return f"{host} unreachable\n"
+        if "Reply from" in result.decode and host not in result.decode:
+            return f"{host} unreachable\n"
+        else:
+            return f"{host} reachable\n"
+    except Exception:
+        return f"Error pinging {host}\n"
+
+def ping_host_linux(host):
+
+    try:
+        result = subprocess.run(['ping', '-c', '4', '-n', host], stdout=subprocess.DEVNULL, encoding='utf-8')
+        if result is not None:
+            return f"{host} reachable\n"
+        else:
+            return f"{host} unreachable\n"
+    except Exception:
+        return f"Error pinging {host}\n"
+
+def ping_sweep(host_file, output_file, max_threads=10):
+
+    tasks = []
+    with open(host_file) as file:
+        for line in file:
+            ip_or_cidr = line.strip()
+            try:
+                address = ipaddress.ip_address(ip_or_cidr)
+                tasks.append(str(address))
+            except ValueError:
+                try:
+                    network = ipaddress.ip_network(ip_or_cidr, strict=False)
+                    tasks.extend(str(host) for host in network.hosts())
+                except ValueError:
+                    print(f"Invalid input: {ip_or_cidr}")
+
+    with ThreadPoolExecutor(max_threads) as executor, open(output_file, "w") as outfile:
+        if platform.system().lower()=='linux':
+            results = executor.map(ping_host_linux, tasks)
+        if platform.system().lower()=='windows':
+            results = executor.map(ping_host_windows, tasks)
+        for result in results:
+            outfile.write(result)
+            print(result)
+
+now = datetime.now()
+formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_now)
+print()
+ping_sweep(host_file, output_file, max_threads=100)
